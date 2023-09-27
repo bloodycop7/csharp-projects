437c47bad04dd248b07c2b939628229d5ecd151e
eon <dobrin_k1@abv.bg>
2023-09-27 22:11:59 +0300

    Initial Commit

---
+++ app/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Testing
+{
+    class Testing
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            if (number >= 0)
+            {
+                Console.WriteLine("Hi");
+            }
+            else
+            {
+                Console.WriteLine("You must input a positive number.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
