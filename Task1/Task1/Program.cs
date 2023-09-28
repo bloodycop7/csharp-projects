e07cef30062ada0bfce9ff95c2d05cec257e3fdf
eon <dobrin_k1@abv.bg>
2023-09-28 15:22:19 +0300

    Task1 Push

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task1
 {
@@ -6,14 +7,40 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            int firstNumber;
             int outputAmount;
-            Console.Write("How many outputs would you like?: ");
+            string[] evenNumbers = { };
 
+            List<string> evenNumList = evenNumbers.ToList();
+
+            Console.Write("What number would you want the other random numbers to check for even: ");
+            string outputParse1 = Console.ReadLine();
+            firstNumber = int.Parse(outputParse1);
+
+            Console.Write("How many outputs would you like: ");
             string outputParse = Console.ReadLine();
-
             outputAmount = int.Parse(outputParse);
 
+            for (int i = 0; i < outputAmount; i++)
+            {
+                if ( ( firstNumber % i ) != 0 )
+                {
+                    Console.WriteLine(i + " is even to " + firstNumber);
 
+                    evenNumList.Add(Convert.ToString(i));
+                }
+                else
+                {
+                    Console.WriteLine(i + " is not even to " + firstNumber);
+                }
+                
+            }
+
+            Console.WriteLine("All even numbers are:");
+            for (int i = 0; i < evenNumList.Count; i++)
+            {
+                Console.WriteLine(evenNumbers.GetValue(i));
+            }
         }
     }
 }
