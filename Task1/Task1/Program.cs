8d76f70e0679ba2eb185185df31bc7fc194e5c78
eon <dobrin_k1@abv.bg>
2023-09-28 15:42:14 +0300

    Finish

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -11,10 +11,6 @@ namespace Task1
             int outputAmount;
             List<string> evenNumList = new List<string>();
 
-            Console.Write("What number would you want the other random numbers to check for even: ");
-            string outputParse1 = Console.ReadLine();
-            firstNumber = int.Parse(outputParse1);
-
             Console.Write("How many outputs would you like: ");
             string outputParse = Console.ReadLine();
             outputAmount = int.Parse(outputParse);
@@ -26,8 +22,9 @@ namespace Task1
                 {
                     int randomNumber = newRandom.Next(0, 100);
 
-                    if ( firstNumber % randomNumber == 0 )
+                    if ( randomNumber % 2 == 0 )
                     {
+
                         evenNumList.Add(Convert.ToString(randomNumber));
                     }
                 }
@@ -35,14 +32,14 @@ namespace Task1
                 {
                     int randomNumber = newRandom.Next(0, 100);
 
-                    if ( firstNumber % randomNumber == 0)
+                    if ( randomNumber % 2 == 0)
                     {
                         evenNumList.Add(Convert.ToString(randomNumber));
                     }
                 }
             }
 
-            Console.WriteLine("All even numbers are:   " + evenNumList.Count);
+            Console.WriteLine("All even numbers are:");
             for (int i = 0; i < evenNumList.Count; i++)
             {
                 Console.WriteLine(evenNumList[i]);
