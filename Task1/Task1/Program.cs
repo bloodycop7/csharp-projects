16d2f758db54d7b55d903647e7d897af84a36f7b
eon <dobrin_k1@abv.bg>
2023-09-28 14:45:54 +0300

    Task1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int outputAmount;
+            Console.Write("How many outputs would you like?: ");
+
+            string outputParse = Console.ReadLine();
+
+            outputAmount = int.Parse(outputParse);
+
+
+        }
+    }
+}
