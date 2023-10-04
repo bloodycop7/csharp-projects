e0a45593fdd6824e9077b3bed2239120c1bec904
eon <dobrin_k1@abv.bg>
2023-10-04 22:16:39 +0300

    HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Task 1
+            int outputVar = (3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3;
+            Console.WriteLine(outputVar); // Gotta make it look clean :D
+
+            Console.ReadKey();*/
+
+            /* Task 2
+            // Мързи ме да пиша WriteLine от 1 до 20
+
+            for (int i = 0; i < 21;  i++)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            /* Task 3
+            int increasedStars = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                for (int i2 = 0; i2 < increasedStars; i2++)
+                {
+                    Console.Write(".");
+                }
+
+                Console.Write(".\n");
+
+                increasedStars++;
+            }
+            */
+        }
+    }
+}
