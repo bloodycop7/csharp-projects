4ca5fae8582d7ac1236ef5121e9e1996d44fc5bd
eon <dobrin_k1@abv.bg>
2023-10-25 11:54:45 +0300

    Update Program.cs

--- Homework3/Task4/Program.cs
+++ Homework3/Task4/Program.cs
@@ -11,6 +11,8 @@
             double width = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Answer: {0}", (length * 2) + (width * 2));
+
+            Console.ReadKey();
         }
     }
 }
