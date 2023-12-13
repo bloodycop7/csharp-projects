352547cf6c9c23e5c7dc5f5b2b9165a0feeb8fb9
eon <dobrin_k1@abv.bg>
2023-12-14 00:12:47 +0200

    TasksByStamba

---
+++ TasksByStamba/Task2/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Party Price: ");
+            double partyPrice = double.Parse(Console.ReadLine());
+            int totalItemCount = 0;
+
+            if ( partyPrice < 1.00 || partyPrice > 10000.00 )
+            {
+                Console.WriteLine("Party Price must be between 1.00 and 10000.00");
+
+                return;
+            }
+
+            Console.Write("Input Love Letters Count: ");
+            int loveLetters = int.Parse(Console.ReadLine());
+
+            if ( loveLetters < 0 || loveLetters > 1000 )
+            {
+                Console.WriteLine("Love Letters must be between 0 and 1000");
+
+                return;
+            }
+
+            totalItemCount = totalItemCount + loveLetters;
+
+            Console.Write("Input Roses made out of Vosuk Count: ");
+            int vosukRozi = int.Parse(Console.ReadLine());
+
+            if ( loveLetters < 0 || loveLetters > 1000 )
+            {
+                Console.WriteLine("Roses made out of Vosuk must be between 0 and 1000.");
+
+                return;
+            }
+
+            totalItemCount = totalItemCount + vosukRozi;
+
+            Console.Write("Input Keychains Count: ");
+            int keyChains = int.Parse(Console.ReadLine());
+
+            if ( keyChains < 0 || keyChains > 1000 )
+            {
+                Console.WriteLine("Keychains must be between 0 and 1000.");
+
+                return;
+            }
+
+            totalItemCount = totalItemCount + keyChains;
+
+            Console.Write("Input Kariakaturi Count: ");
+            int kariakaturi = int.Parse(Console.ReadLine());
+
+            if ( kariakaturi < 0 || kariakaturi > 1000 )
+            {
+                Console.WriteLine("Kariakaturi must be between 0 and 1000.");
+
+                return;
+            }
+
+            totalItemCount = totalItemCount + kariakaturi;
+
+
+            Console.Write("Input Surprise Gifts Count: ");
+            int surpriseGifts = int.Parse(Console.ReadLine());
+
+            if ( surpriseGifts < 0 || surpriseGifts > 1000 )
+            {
+                Console.WriteLine("Surprise Gifts must be between 0 and 1000.");
+
+                return;
+            }
+
+            totalItemCount = totalItemCount + surpriseGifts;
+            double totalCost = ( loveLetters * 0.60 ) + ( vosukRozi * 7.20 ) + ( keyChains * 3.60 ) + ( kariakaturi * 18.20 ) + ( surpriseGifts * 22 );
+
+            if ( totalItemCount >= 25 )
+            {
+                totalCost = totalCost - ( totalCost * 0.35 );
+            }
+
+            totalCost = totalCost - ( totalCost * 0.10 );
+            totalCost = totalCost - partyPrice;
+
+            Console.WriteLine(totalCost);
+        }
+    }
+}
---
+++ TasksByStamba/Task3/Program.cs
@@ -0,0 +1,108 @@
+﻿using System .Data;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nights = int.Parse(Console.ReadLine());
+            string destination = Console.ReadLine();
+            string typeRoom = Console.ReadLine();
+            string packageType = Console.ReadLine();
+
+            if ( destination != "Paris" || destination != "Phuket" || destination != "Mexico" )
+            {
+                Console.WriteLine("Destination must be Paris, Phuket or Mexico!");
+
+                return;
+            }
+
+            if ( typeRoom != "apartment" || typeRoom != "double room" )
+            {
+                Console.WriteLine("Room Type must be apartment or double room!");
+
+                return;
+            }
+
+            if ( packageType != "apartment" || packageType != "double room" )
+            {
+                Console.WriteLine("Package Type must be regular, all-inclusive or ultra-all-inclusive!");
+
+                return;
+            }
+
+            double price = 0;
+
+            switch (destination)
+            {
+                case "Paris":
+                    switch (typeRoom)
+                    {
+                        case "double room":
+                            price = price + 150;
+
+                            break;
+                        case "apartment":
+                            price = price + 300;
+
+                            break;
+                        default:
+                            break;
+                    }
+
+                    break;
+                case "Phyket":
+                    switch (typeRoom)
+                    {
+                        case "double room":
+                            price = price + 250;
+
+                            break;
+                        case "apartment":
+                            price = price + 360;
+
+                            break;
+                        default:
+                            break;
+                    }
+
+                    break;
+                case "Mexico":
+                    switch (typeRoom)
+                    {
+                        case "double room":
+                            price = price + 220;
+
+                            break;
+                        case "apartment":
+                            price = price + 310;
+
+                            break;
+                        default:
+                            break;
+                    }
+
+                    break;
+            }
+
+            switch (packageType)
+            {
+                case "all-inclusive":
+                    price = price + ( price * 0.3 );
+
+                    break;
+                case "ultra-all-inclusive":
+                    price = price + ( price * 0.5 );
+
+                    break;
+                case "regular":
+                    break;
+            }
+
+            price = ( price * nights );
+
+            Console.WriteLine($"Total Price: {price:F2}");
+        }
+    }
+}
