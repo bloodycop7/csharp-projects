1baa6524842c1e0d30839c5d762787d3d464a760
eon <dobrin_k1@abv.bg>
2024-03-20 23:37:10 +0200

    Delete stored files

--- ConsoleApp1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Windows;
-using System.Windows.Forms;
-
-for (int i = 0; i < 10; i++)
-{
-    Clipboard.SetText("Hello, clipboard!");
-}
--- Loops/Task7/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Task7 {
-    internal class Program {
-        static void Main(string[] args) {
-            // Ms. Teacher, It is 11:33 PM, I am tired and cannot bother doing the rest of them, I don't even understand what I'm reading. Sorry :(
-            // Please Forgive!
-        }
-    }
-}
